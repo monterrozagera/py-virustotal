b007e2520c6f33a9fa814904ecc8471461bf99fb
monterrozagera <monterrozagerardo@protonmail.com>
2022-10-16 02:37:07 -0700

    add main functions

--- python-virustotal.py
+++ python-virustotal.py
@@ -1,24 +1,25 @@
+from hashlib import sha1
+from time import sleep
 import argparse
-import hashlib
+
 import requests
-import json
 
 class VT_Handler():
 
     def __init__(self, api_key):
         self.key = api_key
 
-    def printKey(self):
+    def printKey(self) -> None:
         """ prints loaded API key """
         print(f"API key: {self.key}")
 
-    def printFile(self, file):
+    def printFile(self, file) -> None:
         """ prints loaded file to analyze """
         print(f"Loaded file: {file} (Hash: {self.getFileHash(file)})")
 
     def getFileHash(self, file_path) -> str:
         """ returns SHA-1 digest of a file """
-        hash_lib = hashlib.sha1()
+        hash_lib = sha1()
 
         with open(file_path, 'rb') as file:
 
@@ -30,8 +31,9 @@ class VT_Handler():
             return hash_lib.hexdigest()
 
 
-    def hashLookup(self, hash_sample) -> dict:
-        """ request for hash information """
+    def hashLookup(self, hash_sample) -> None:
+        """ sends GET request to gather information about hash """
+        " TODO: add more outputz "
 
         url = f"https://www.virustotal.com/api/v3/search?query={hash_sample}"
 
@@ -57,9 +59,46 @@ class VT_Handler():
             for tag in tags:
                 print('\t' + tag)
 
-                
+    def uploadFile(self, file_to_upload) -> str:
+        """ uploads file to VirusTotal, returns report ID """
+        url =  "https://www.virustotal.com/api/v3/files"
+        headers = {
+            'accept': 'application/json',
+            'x-apikey': self.key
+        }
+        files = {'file': open(file_to_upload, 'rb')}
 
+        response = requests.post(url, files=files, headers=headers)
+        contents = response.json()
+        return contents['data']['id']
 
+    def getFileAnalysis(self, id):
+        """ prints results of file analysis """
+        url = f"https://www.virustotal.com/api/v3/analyses/{id}"
+        headers = {
+            "accept": "application/json",
+            "x-apikey": self.key
+        }
+
+        response = requests.get(url, headers=headers)
+        contents = response.json()
+
+        while contents['data']['attributes']['status'] == 'queued':
+            sleep(5)
+            print("[!] Report status: queued. Waiting..")
+            response = requests.get(url, headers=headers)
+            contents = response.json()
+
+        if contents['data']['attributes']['status'] == 'completed':
+            stats = contents['data']['attributes']['stats']
+
+            print("[!] Stats: ")
+            for attribute, value in stats.items():
+                print(f"\t{attribute} = {value}")
+
+    def createReport(self):
+        """ creates PDF report file """
+        pass
 
 
 if __name__ == '__main__':
@@ -83,3 +122,5 @@ if __name__ == '__main__':
 
     hash = virus_total.getFileHash(file)
     virus_total.hashLookup(hash)
+    id = virus_total.uploadFile(file)
+    virus_total.getFileAnalysis(id=id)
